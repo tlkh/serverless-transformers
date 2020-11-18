d7514dce131c4187d79dce9126f9c7e705e54f5c
Timothy Liu <tlkh.xms@gmail.com>
2020-11-18 16:59:33 +0800

    added evidences

--- app.py
+++ app.py
@@ -10,9 +10,11 @@ DEFAULT_URL = "https://en.wikipedia.org/wiki/Nvidia"
 DEFAULT_ENDPOINT = "https://cloudrun-transformer-test-34x4ouuclq-de.a.run.app/predict"
 
 st.sidebar.markdown("# Transformer on Cloud Run")
+st.sidebar.markdown("**Model**")
+st.sidebar.markdown("`allenai/unifiedqa-t5-base` (220M)")
 st.sidebar.markdown("**Options**")
 
-max_words = st.sidebar.slider("Max Word Count", min_value=128, max_value=1024, value=512, step=32)
+max_words = st.sidebar.slider("Max Doc Word Count", min_value=128, max_value=1024, value=512, step=32)
 
 @st.cache
 def download_article(article_url):
@@ -37,6 +39,7 @@ def main():
     article_text = download_article(article_url)
     article_text = " ".join(article_text.split(" ")[:max_words])
     context = article_text
+    sentences = context.lower().split(".")
     with st.beta_expander(label="Show Text Document", expanded=False):
         st.markdown(article_text)
     question = st.text_input("Question", DEFAULT_QUESTION)
@@ -46,6 +49,11 @@ def main():
     try:
         answer = response["answer"][0]
         st.markdown("### "+answer)
+        st.markdown("Evidence:")
+        answer = answer.lower()
+        for s in sentences:
+            if answer in s:
+                st.markdown("* "+s.replace("\n", "").strip())
     except:
         st.markdown(response)
 
